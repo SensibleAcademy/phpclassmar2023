b3ee103543692591f7d6a39d145d32c970160777
sensible <sensiblemarg@gmail.com>
2023-05-08 08:57:57 +0530

    array concepts

--- second.php
+++ second.php
@@ -4,27 +4,70 @@
 
     <?php 
    
+   /*
      $a = $_REQUEST["t1"];
      $b = $_REQUEST["t2"];
      $c = $_REQUEST["t3"];
+*/
 
- 
-function show()   //Non Parametrized function definition
-     {
-        print("<br>Hello");
-        print("<br>How are you");
-        print("<br>I am fine");
-        print("<br>What about you ?");
-     }
-  function add($x,$y)   // Parametrized function definition
-     {
-         $z=$x + $y;
-         print("<br>Addition is $z");
-     }
+   $a=array(10,20,30.9,"ram",50);
 
-  show();    //function calling
-      add(4,7);
-      add($a,$b);
+   //print(count($a));
+   //print($a[-1]);
+
+   /*
+   for($i=0;$i<=count($a)-1;$i++)
+   {
+      echo($a[$i]." ");
+   }
+   */
+
+   /*
+   foreach($a as $b)
+   {
+      echo($b."<br>");
+   }
+   */
+
+//   print_r($b);
+
+
+// ASSOCIATIVE ARRAY
+
+$m=array("a"=>10,"b"=>20,"c"=>30,"d"=>40,50);
+
+//print(count($m));
+/* ERROR
+for($i=0;$i<=count($m)-1;$i++)
+   {
+      echo($m[$i]." ");
+   }
+
+   */
+/*
+   foreach($m as $b=>$c)
+   {
+      echo($b." -> " . $c. "  , " );
+   }
+*/
+
+// 2-d array 
+
+$a1=array(array(10,20,30),array(10,20,30,40),array(60,70));
+
+for($i=0;$i<=count($a1)-1;$i++)
+{
+   for($j=0;$j<=count($a1[$i])-1;$j++)
+   {
+       print($a1[$i][$j]."  ");
+   }
+   print("<br>");
+}
+
+
+
+   
+
     ?>
   
     </h1>
