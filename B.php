66a17590b20c18067b8a8b8a2e11ad291ea81528
sensible <sensiblemarg@gmail.com>
2023-05-10 09:11:08 +0530

    header , link data transfer and super global functions

--- B.php
+++ B.php
@@ -1,18 +1,9 @@
-<?php session_start(); ?>
-<html>
-<body>
-    <h1>I am B </h1>
-    <?php 
-        $_SESSION["info"]=$_REQUEST["t1"];
+<?php session_start(); ?><?php 
+    
+    
+    $_SESSION["info"]=$_REQUEST["t1"];
+    $a=$_SESSION["info"];
+          
+          header("location:C.php?txt=$a");
 
     ?>
-
-    <form method="get" action="C.php">
-        Enter second number <input type="text" name="t2"> <br>
-        <br>
-
-
-        <input type="submit" value="Ok">
-     </form>   
-</body>
-</html>
--- C.php
+++ C.php
@@ -2,6 +2,36 @@
 <body>
     <h1>I am C </h1>
 
+<?php 
+   
+   /*
+   echo($_SERVER['PHP_SELF']."<br>");
+
+   echo($_SERVER['SERVER_NAME']."<br>");
+
+   echo($_SERVER['HTTP_HOST']."<br>");
+
+
+   $nm="sonu";
+   function show()
+   {
+    $GLOBALS["nm"]="monu";
+        echo("<br>");
+         echo($GLOBALS["nm"]);
+         echo("<br>");
+
+          
+   }
+   echo("$nm");
+   echo("<br>");
+   show();
+   echo("<br>");
+   echo("$nm");
+
+   */
+
+?>
+
     <form method="get" action="D.php">
 
         Enter third number <input type="text" name="t3"> <br>
